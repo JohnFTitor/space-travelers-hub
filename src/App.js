54ff7fd3d2e0b319112f279c4f5696d63ff29953
lisandroseia <lisandroseia11@gmail.com>
2022-02-28 19:09:56 -0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './styles/App.scss';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import MyProfile from './components/MyProfile';
+import Missions from './components/Missions';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Routes>
           <Route exact path="/" element={<h1>I&aphos;m the main page </h1>} />
+          <Route exact path="missions" element={<Missions />} />
           <Route exact path="/profile" element={<MyProfile />} />
         </Routes>
       </main>
