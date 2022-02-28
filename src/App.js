0888c8b5498b9522c897c5565d0873ef422e2e63
lisandroseia <lisandroseia11@gmail.com>
2022-02-28 16:31:36 -0300

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
       </header>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => (
-    <>
+  <>
     <nav>
       <h1>Space travelers' hub</h1>
+      {/* <ul>
+        <li>
+          <Link>Rockets</Link>
+        </li>
+        <li>
+          <Link>Missions</Link>
+        </li>
+        <li>
+          <Link>My profile</Link>
+        </li>
+      </ul> */}
     </nav>
-    </>
-)
+  </>
+);
 
+export default NavBar;
