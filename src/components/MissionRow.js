35eb8debee0602916d6d4ba58111ba0706562b1f
lisandroseia <lisandroseia11@gmail.com>
2022-03-01 19:15:13 -0300

    Changes reserve to true

--- src/components/MissionRow.js
+++ src/components/MissionRow.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../redux/missions/missions';
 
 const MissionRow = (props) => {
-  const { name, description } = props;
+  const { name, description, id } = props;
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    dispatch(joinMission(e.target.id));
+  };
 
   return (
     <tr>
       <th>{name}</th>
       <th>
-        <p>
-          {description}
-        </p>
+        <p>{description}</p>
         <br />
       </th>
       <th className="badge">
         <span>not a member</span>
       </th>
       <th className="join">
-        <button type="button">
+        <button onClick={(e) => handleClick(e)} id={id} type="button">
           Join Mission
         </button>
       </th>
@@ -30,4 +35,5 @@ export default MissionRow;
 MissionRow.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
