c8c33bd0e83a24e60dae9a8a4b061ca75d1f2c19
lisandroseia <lisandroseia11@gmail.com>
2022-03-01 13:06:11 -0300

    Fetch rockets when page loads

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './styles/App.scss';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+import './styles/App.scss';
 import NavBar from './components/NavBar';
 import MyProfile from './components/MyProfile';
 import RocketsPage from './components/RocketsPage';
 import Missions from './components/Missions';
+import { addRocket } from './redux/rockets/rockets';
 
 function App() {
+  const url = 'https://api.spacexdata.com/v3/rockets';
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data.forEach((item) => {
+        const newRocket = {
+          id: item.id,
+          rocket_name: item.rocket_name,
+          description: item.description,
+          flickr_images: item.flickr_images,
+        };
+        dispatch(addRocket(newRocket));
+      }));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/reset.scss';
 import './styles/index.scss';
 import App from './App';
+import store from './redux/configStore';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
