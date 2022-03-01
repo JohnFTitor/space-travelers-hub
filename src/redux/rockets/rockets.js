74e47cb611fed327e1c027fbd7608b718030fae3
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-03-01 16:26:38 -0500

    Define reducer logic for updating reserve bool

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,14 +1,38 @@
 const ADD_ROCKET = 'spaceTravelersHub/rockets/ADD_ROCKET';
+const RESERVE = 'spaceTravelersHub/rockets/RESERVE';
 
 const initialState = [];
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ROCKET:
+      return [...state, action.payload];
+    case RESERVE: {
+      const newState = [...state];
+      newState[action.payload - 1].reserved = true;
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
 
 const addRocket = (payload) => ({
   type: ADD_ROCKET,
   payload,
 });
 
+const reserveRocket = (id) => ({
+  type: RESERVE,
+  payload: id,
+});
+
+export { reserveRocket };
+
 export function filterRocketInfo() {
-  return function (dispatch) {
+  return (dispatch) => {
     const url = 'https://api.spacexdata.com/v3/rockets';
     fetch(url)
       .then((res) => res.json())
@@ -18,19 +42,9 @@ export function filterRocketInfo() {
           rocket_name: item.rocket_name,
           description: item.description,
           flickr_images: item.flickr_images[0],
+          reserved: false,
         };
         dispatch(addRocket(newRocket));
       }));
   };
 }
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_ROCKET:
-      return [...state, action.payload];
-    default:
-      return state;
-  }
-};
-
-export default reducer;
